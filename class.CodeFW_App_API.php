d7063b03bb6559a9a3815ec751ee12baaca375e7
João Patrício <jmpatricio@gmail.com>
2014-02-09 17:15:18 +0000

    Working local

---
+++ class.CodeFW_App_API.php
@@ -0,0 +1,47 @@
+<?php
+
+error_reporting(E_ALL);
+
+/**
+ * Abstract class for any CodeFW app api.
+ * Each api should be extend this class in order to run correctly in codefw
+ *
+ * @author Joao Patricio
+ * @since 1.0
+ * @version 1.0
+ */
+
+if (0 > version_compare(PHP_VERSION, '5')) {
+    die('This file was generated for PHP 5');
+}
+
+/* user defined includes */
+// section 127-0-1-1--673d1cc1:14403cfd6a5:-8000:0000000000000B05-includes begin
+// section 127-0-1-1--673d1cc1:14403cfd6a5:-8000:0000000000000B05-includes end
+
+/* user defined constants */
+// section 127-0-1-1--673d1cc1:14403cfd6a5:-8000:0000000000000B05-constants begin
+// section 127-0-1-1--673d1cc1:14403cfd6a5:-8000:0000000000000B05-constants end
+
+/**
+ * Abstract class for any CodeFW app api.
+ * Each api should be extend this class in order to run correctly in codefw
+ *
+ * @abstract
+ * @access public
+ * @author Joao Patricio
+ * @since 1.0
+ * @version 1.0
+ */
+abstract class CodeFW_App_API
+{
+    // --- ASSOCIATIONS ---
+
+
+    // --- ATTRIBUTES ---
+
+    // --- OPERATIONS ---
+
+} /* end of abstract class CodeFW_App_API */
+
+?>
